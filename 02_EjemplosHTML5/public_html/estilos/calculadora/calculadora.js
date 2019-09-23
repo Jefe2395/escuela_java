744166a6b26617b4a0ea3416fa597ed73c4b9c38
Jaime Palomo <jeja2279@gmail.com>
2019-09-23 16:55:13 +0200

    Se añade mejoras a la calculadora

--- 02_EjemplosHTML5/public_html/estilos/calculadora/calculadora.js
+++ 02_EjemplosHTML5/public_html/estilos/calculadora/calculadora.js
@@ -3,9 +3,18 @@ class Calculadora
     constructor()
     {
         this.result = document.getElementById("resultado");
+        this.anterior = document.getElementById("anterior")
+        this.men = 0;
+        this.operador = "";
+        this.nuevoNum = false;
     }
 
     numeroPulsado(eventObj) {
+        if (this.nuevoNum)
+        {
+            this.result.value = "0";
+            this.nuevoNum = false;
+        }
         let valor = eventObj.currentTarget.innerHTML;
         //alert("Pulsado " + valor);
         if (valor === "+/-")
@@ -17,7 +26,45 @@ class Calculadora
                 this.result.value += valor;
             }
         } else
+        {
             this.result.value += valor;
+            this.result.value = parseFloat(this.result.value);
+        }
+    }
+    operadorPulsado(evObj)
+    {
+        let operadorActual = evObj.currentTarget.innerHTML;
+
+        if (this.operador !== "" || operadorActual === "=")
+        {
+            this.calcular();
+        }
+
+        this.men = parseFloat(this.result.value);
+        //Subir a caja de texto valor anterior y operador
+
+        if (this.operador !== "=" && operadorActual !== "=")
+        {
+            this.anterior.value = `${this.men} ${this.operador}`;
+            this.result.value = "0";
+            this.operador = operadorActual;
+        } else
+        {
+            this.operador = "";
+
+        }
+
+        this.nuevoNum = true;
+    }
+    calcular()
+    {
+        if (this.operador !== "=")
+        {
+            let valActual = parseFloat(this.result.value);
+            let resultado = eval(this.men.toString() + this.operador + valActual);
+            this.result.value = resultado;
+        }
+
     }
 }
 ;
@@ -25,12 +72,20 @@ let calculadora = null;
 window.onload = function ()
 {
     let botones = document.getElementsByClassName("num"); //Array de botones
+    let botonesOp = document.getElementsByClassName("oper");
     calculadora = new Calculadora();
     for (let boton of botones)
     {
-        boton.addEventListener("click", (evtObj) => 
+        boton.addEventListener("click", (evtObj) =>
         {
             calculadora.numeroPulsado(evtObj)
+        });
+    }
+    for (let botOp of botonesOp)
+    {
+        botOp.addEventListener("click", (evObj) =>
+        {
+            calculadora.operadorPulsado(evObj);
         });
     }
 };
