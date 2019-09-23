99c2c9a2d692c557910889b547517bff84d614d3
Jaime Palomo <jeja2279@gmail.com>
2019-09-23 15:12:58 +0200

    Completamos la calculadora de varias maneras

---
+++ 02_EjemplosHTML5/public_html/estilos/calculadora/calculadora.js
@@ -0,0 +1,39 @@
+class Calculadora
+{
+    constructor()
+    {
+        this.result = document.getElementById("resultado");
+    }
+
+    numeroPulsado(eventObj) {
+        let valor = eventObj.currentTarget.innerHTML;
+        //alert("Pulsado " + valor);
+        if (valor === "+/-")
+            this.result.value = "" + (-(parseFloat(this.result.value)));
+        else if (valor === ".")
+        {
+            if (!this.result.value.includes("."))
+            {
+                this.result.value += valor;
+            }
+        } else
+            this.result.value += valor;
+    }
+}
+;
+let calculadora = null;
+window.onload = function ()
+{
+    let botones = document.getElementsByClassName("num"); //Array de botones
+    calculadora = new Calculadora();
+    for (let boton of botones)
+    {
+        boton.addEventListener("click", (evtObj) => 
+        {
+            calculadora.numeroPulsado(evtObj)
+        });
+    }
+};
+
+
+
