53f004f6e63ad4586eca2803f80269cbfdc15e53
Jaime Palomo <jeja2279@gmail.com>
2019-09-25 09:56:05 +0200

    Jaime

---
+++ 01_EjemploJava/test/pruebas/TestColab.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import threads.HiloFichero;
+import threads.StrAuxV1;
+import threads.StrAuxV2;
+import threads.StrAuxV3;
+
+/**
+ *
+ * @author usuario
+ */
+public class TestColab {
+    
+    public TestColab() {
+    }
+
+}
