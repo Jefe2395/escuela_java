109752d15bf6edfd1d08ef929afa2e3004abc99c
Jaime Palomo <jeja2279@gmail.com>
2019-09-27 16:50:05 +0200

    Hacemos el Servlet

--- Ahorcado/src/java/com/sinensia/controlador/ControladorAhorcado.java
+++ Ahorcado/src/java/com/sinensia/controlador/ControladorAhorcado.java
@@ -26,16 +26,8 @@ public class ControladorAhorcado extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ControladorAhorcado</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ControladorAhorcado at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            Juego jue= new Juego();
+            request.getSession().setAttribute("palabra", jue.getPalabra());
         }
     }
 
