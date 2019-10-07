4ddaa12c3faa6e749d444703d1716f05dc0b234e
Jaime Palomo <jeja2279@gmail.com>
2019-10-07 10:18:04 +0200

    Añadimops los detalles a cada heroe

--- 07_Angular/una-web/src/app/hero-detail/hero-detail.component.ts
+++ 07_Angular/una-web/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Hero} from '../model/hero';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  // Esta propiedad viene de algún compoonente externo
+  @Input()
+  hero: Hero;
 
   constructor() { }
 
